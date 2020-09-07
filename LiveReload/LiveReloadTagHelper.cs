7bc9133833aae91a26269085a6bd8c4f727b9337
Ben Dornis <ben@buildstarted.com>
2020-09-07 00:08:28 -0700

    added attributes to override some default options on a per page basis

--- LiveReload/LiveReloadTagHelper.cs
+++ LiveReload/LiveReloadTagHelper.cs
@@ -17,6 +17,16 @@ namespace LiveReload
             this.options = options.Value;
         }
 
+        [HtmlAttributeName("show-status-on-page")]
+        public bool? ShowStatusOnPage { get; set; }
+        [HtmlAttributeName("reload-on-reconnect")]
+        public bool? ReloadOnReconnect { get; set; }
+        [HtmlAttributeName("inline-updates-when-possible")]
+        public bool? InlineUpdatesWhenPossible { get; set; }
+        [HtmlAttributeName("save-form-data")]
+        public bool? SaveFormData { get; set; }
+
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             try
@@ -26,8 +36,19 @@ namespace LiveReload
                 randomid = "?" + Guid.NewGuid().ToString().Substring(0, 8);
 #endif
 
+                //options override
+                //show status
+                //Show-Status-On-Page
+                var localoptions = new LiveReloadOptions()
+                {
+                    SaveFormData = SaveFormData ?? options.SaveFormData,
+                    ReloadOnReconnect = ReloadOnReconnect ?? options.ReloadOnReconnect,
+                    ShowStatusOnPage = ShowStatusOnPage ?? options.ShowStatusOnPage,
+                    InlineUpdatesWhenPossible = InlineUpdatesWhenPossible ?? options.InlineUpdatesWhenPossible
+                };
+                
                 output.TagName = null;
-                output.Content.SetHtmlContent($"<script>{options.ToJavascriptString()}</script><script src='{options.LiveReloadLocalScriptPath}{randomid}'></script>");
+                output.Content.SetHtmlContent($"<script>{localoptions.ToJavascriptString()}</script><script src='{options.LiveReloadLocalScriptPath}{randomid}'></script>");
             }
             catch (Exception e)
             {
